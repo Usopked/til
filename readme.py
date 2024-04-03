e8e04b862236ee3e507a802e4a9c4aa1f49c7cd1
Usopked <usopked16496@gmail.com>
2024-04-03 23:52:25 +0900

    경로수정2

--- readme.py
+++ readme.py
@@ -5,7 +5,7 @@ readme_path = './README.md'
 folders = [f for f in os.listdir(til_folder_path) if os.path.isdir(os.path.join(til_folder_path, f))]
 readme_text = '# Today I Learned\n\n## Topics\n\n'
 for folder in folders:
-    link = f'- [{folder}]({os.path.join("TIL", folder)})\n'
+    link = f'- [{folder}]({os.path.join("til", folder)})\n'
     readme_text += link
 
 with open(readme_path, 'w') as readme_file:
