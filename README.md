fbb50aec8fe14cfd1777e4433068541d9c6b8d08
Usopked <usopked16496@gmail.com>
2024-04-03 23:47:44 +0900

    readme 수정

--- README.md
+++ README.md
@@ -1,8 +1,2 @@
 # til
 today i learned
-
-```
-with open(readme_path, 'w') as readme_file:
-    readme_file.write(readme_text)
-print("README.md has been updated with links to TIL subfolders.")
-```
--- readme.py
+++ readme.py
@@ -7,3 +7,7 @@ readme_text = '# Today I Learned\n\n## Topics\n\n'
 for folder in folders:
     link = f'- [{folder}]({os.path.join("TIL", folder)})\n'
     readme_text += link
+
+with open(readme_path, 'w') as readme_file:
+    readme_file.write(readme_text)
+print("README.md has been updated with links to TIL subfolders.")
